00ad911b58b256b7369788739a774833fb6461ca
Jose Tomas Silva <jsilva5@miuandes.cl>
2018-03-27 12:28:06 -0300

    cambios

--- MyApplication4/app/src/main/java/com/example/ing/myapplication/MainActivity.java
+++ MyApplication4/app/src/main/java/com/example/ing/myapplication/MainActivity.java
@@ -12,6 +12,10 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button b;
+    private TextView t;
+    private TextView t2;
+
     private DrawerLayout mDrawerLayout;
 
     public static final String MY_PREFS_NAME = "MyPrefsFile";
@@ -20,6 +24,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        t= findViewById(R.id.textView);
+        t2= findViewById(R.id.textView2);
+        b = findViewById(R.id.Button);
+        SharedPreferences prefs = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE);
+        String restoredText = prefs.getString("email", null);
+        if (restoredText != null) {
+            String email = prefs.getString("email", "");
+            String pass = prefs.getString("pass", "");
+            t.setText("name:"+email);
+            t2.setText("password:"+pass);
+        }
+        else
+        {
+            Intent intent = new Intent(this, Login.class);
+            startActivityForResult(intent,1);
+        }
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Logout();
+            }
+        });
+
+        //Nuevo Lab 2
         mDrawerLayout = findViewById(R.id.drawer_layout);
 
         NavigationView navigationView = findViewById(R.id.nav_view);
@@ -38,6 +66,34 @@ public class MainActivity extends AppCompatActivity {
                         return true;
                     }
                 });
+
+    }
+
+    public void Logout(){
+        t.setText("");
+        t2.setText("");
+        SharedPreferences.Editor editor = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE).edit();
+        editor.clear();
+        editor.apply();
+        Intent intent = new Intent(this, Login.class);
+        startActivityForResult(intent,1);
+    }
+
+    @Override
+    protected void onActivityResult(int request, int result, Intent data)
+    {
+        super.onActivityResult(request, result, data);
+        if(request==1)
+        {
+            String email=data.getStringExtra("M");
+            String pass=data.getStringExtra("P");
+            SharedPreferences.Editor editor = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE).edit();
+            editor.putString("email", email);
+            editor.putString("pass", pass);
+            editor.apply();
+            t.setText("name:"+email);
+            t2.setText("password:"+pass);
+        }
 
     }
 }
